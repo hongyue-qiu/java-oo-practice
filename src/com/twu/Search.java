48381d488ac0fcb5e9bae57cc1b3c621c0711a35
hongyue-qiu <994059641@qq.com>
2020-09-01 19:40:42 +0800

    [hongyue-qiu] fix= pix accessing property test.

---
+++ src/com/twu/Search.java
@@ -0,0 +1,28 @@
+package com.twu;
+
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class Manager{
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    String name = "admin";
+    String password = "admin749";
+
+
+
+}
